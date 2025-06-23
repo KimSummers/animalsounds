b2aaaf8c709e16bd8cedbc1bdb8f3a1a004ecb18
Kim Summers <15609753+KimSummers@users.noreply.github.com>
2025-06-23 13:25:52 +0100

    Documenting functions

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,12 @@
+#' What sounds the animal makes
+#'
+#' @param animal string
+#' @param sound string
+#'
+#' @return A string stating what sound the animal makes
+#' @export
+#'
+#' @examples animal_sounds("World", "hello")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
