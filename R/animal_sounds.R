53d435514abe49f9b622c2ba0998181a62597887
Kim Summers <15609753+KimSummers@users.noreply.github.com>
2025-06-24 10:57:31 +0100

    Better error handling

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -15,28 +15,42 @@ animal_sounds <- function(animal, sound = NULL) {
 
   if (!rlang::is_character(animal, n = 1)) {
     cli_abort(
-      c("{.var animal} must be a single string!",
+      c("{.var animal} must be a {.cls character} vector of length one!",
         "i" = "It was {.type {animal}} of length {length(animal)} instead.",
         "i" = "You typed {animal}."),
       class = "error_not_single_string"
-      )
+    )
+
   }
 
   if (is.null(sound))
   {
-    paste0("The ", animal, " makes no sound.")
-  }else
-  {
-    if (!rlang::is_character(sound, n = 1)) {
-      cli_abort(
-        c("{.var sound} must be a {.cls character} vector of length one!",
-          "i" = "It was {.type {sound}} of length {length(sound)} instead.",
-          "i" = "You typed {sound}."),
-        class = "error_not_single_string"
-      )
-    }
-
-    paste0("The ", animal, " says ", sound, "!")
+    return(paste0("The ", animal, " makes no sound."))
   }
 
+  if (!rlang::is_character(sound, n = 1)) {
+    cli_abort(
+      c("{.var sound} must be a {.cls character} vector of length one!",
+        "i" = "It was {.type {sound}} of length {length(sound)} instead.",
+        "i" = "You typed {sound}."),
+      class = "error_not_single_string"
+    )
+  }
+
+  paste0("The ", animal, " says ", sound, "!")
 }
+
+check_arg <- function(arg, n = 1){
+
+  if (!rlang::is_character(arg, n = n)){
+    cli_abort(
+      c("{.var {rlang::caller_arg(arg)}} must be a single string!",
+      "i" = "It was {.type {arg}} of length {length(animal)} instead.",
+      "i" = "You typed {arg}."),
+      call = rlang::caller_env(),
+      class = "error_not_single_string"
+    )
+    }
+
+}
+
